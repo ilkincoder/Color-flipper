ddaaa6a30cdd41b0e5f9a0e18c8c87f736b6ef3a
Ilkin Hamzayev <ilkinhamzayev925@gmail.com>
2024-02-06 22:00:11 +0100

    Simple Flipper Done

---
+++ script.js
@@ -0,0 +1,38 @@
+const colors = ['green' , 'red', 'blue', 'rgba(133, 122, 200)']
+
+// Getting dom 
+
+const btn = document.getElementById('btn');
+
+const color = document.querySelector('.color');
+
+
+
+btn.addEventListener('click', function(){
+    let randomNumber = getRandomNumber();
+    document.body.style.backgroundColor = colors[randomNumber];
+    color.textContent = colors[randomNumber];
+
+})
+
+
+
+//  Function to create random color
+function getRandomNumber(){
+
+    return Math.floor(Math.random() *4);
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
